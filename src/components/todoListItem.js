8e256246ea033644e2585360f578991524d84c39
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-14 13:24:35 -0700

    starting edit todo

--- src/components/todoListItem.js
+++ src/components/todoListItem.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class TodoListItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    };
+  }
+  
   render() {
     const { todo, deleteTodo, id } = this.props;
 
